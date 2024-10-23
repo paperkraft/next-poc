df020111899687c5b4db623bb4083c37ee45a6e7
paperkraft <royalsv105@gmail.com>
2024-10-23 20:48:17 +0530

    props

--- src/components/custom/auth-provider.tsx
+++ src/components/custom/auth-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { SessionProvider } from "next-auth/react";
-import { ChildProps } from "@/types/children";
+import { ChildProps } from "@/types/types";
 
 export const NextAuthProvider = ({ children }:ChildProps) => {
   return <SessionProvider>{children}</SessionProvider>;

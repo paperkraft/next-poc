273a0a6df1b3d5a2ee5ced3ff2d7a59ea8ce5b98
paperkraft <royalsv105@gmail.com>
2024-10-23 20:28:58 +0530

    layout

--- src/components/custom/layout/AppLayout.tsx
+++ src/components/custom/layout/AppLayout.tsx
@@ -1,24 +1,28 @@
 'use client'
 import React from "react";
-import { useSession } from "next-auth/react";
-import { redirect, usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import LeftSidebar from "./LeftSidebar";
+import { useSession } from "next-auth/react";
 
-const publicURL = ['/signin', '/signup'];
+export const publicURL = ['/signin', '/signup'];
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
+  const route = useRouter();
   const pathname = usePathname();
   const isPublicURL = publicURL.includes(pathname);
+
+  const { data: session } = useSession();
+  const user = session?.user;
 
   useSession({
     required: true,
     onUnauthenticated() {
-      !isPublicURL && redirect('/')
+      !isPublicURL && route.push('/')
     }
   });
 
-  if(isPublicURL){
-    return children
+  if (isPublicURL || (pathname === "/" && !user)) {
+    return <React.Fragment>{children}</React.Fragment>
   } else {
     return <LeftSidebar>{children}</LeftSidebar>
   }

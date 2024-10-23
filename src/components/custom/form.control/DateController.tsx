bb85217cc7869f88541c2f2bd12042fed7975b88
paperkraft <royalsv105@gmail.com>
2024-10-23 20:50:01 +0530

    Date controller - updated

--- src/components/custom/form.control/DateController.tsx
+++ src/components/custom/form.control/DateController.tsx
@@ -1,15 +1,14 @@
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { HTMLAttributes } from "react"
-import { Control, Controller, FieldValues, Path, PathValue, useFormContext } from "react-hook-form"
+import { FieldValues, Path, PathValue, useFormContext } from "react-hook-form"
 import { DateField } from "./DateField";
 import { isDate } from "date-fns";
 
 interface IInputControllerProps<T extends FieldValues>
-extends HTMLAttributes<HTMLInputElement> {
-    control?: Control<T>;
+    extends HTMLAttributes<HTMLInputElement> {
     name: Path<T>;
     label: string;
-    description?:string;
+    description?: string;
     placeholder?: string;
     defaultValue?: PathValue<T, Path<T>> | undefined;
     icon?: React.ReactNode;
@@ -19,33 +18,35 @@ extends HTMLAttributes<HTMLInputElement> {
     toYear?: number;
 }
 
-export const DateController = <T extends FieldValues>({control, name, label, ...rest}:IInputControllerProps<T>) => {
+export const DateController = <T extends FieldValues>({ name, label, ...rest }: IInputControllerProps<T>) => {
     const form = useFormContext();
     return (
         <FormField
             control={form.control}
             name={name}
-            render={({field, fieldState:{error}})=>(
+            render={({ field }) => (
                 <FormItem className="w-full">
                     <FormLabel>{label}</FormLabel>
                     <FormControl>
                         <DateField
+                            name={name}
                             value={field.value ? new Date(field.value) : null}
                             onChange={(date: Date | null) => {
                                 try {
                                     field.onChange(isDate(date) ? date.toISOString() : '');
                                 } catch (error) {
                                     console.error(error);
+                                    throw error;
                                 }
                             }}
-                            disabled={rest.disabled}
-                            readOnly={rest.readOnly}
-                            fromYear={rest.fromYear}
-                            toYear={rest.toYear}
+                            disabled={rest?.disabled}
+                            readOnly={rest?.readOnly}
+                            fromYear={rest?.fromYear}
+                            toYear={rest?.toYear}
                         />
                     </FormControl>
                     <FormDescription>{rest?.description}</FormDescription>
-                    <FormMessage>{error?.message}</FormMessage>
+                    <FormMessage />
                 </FormItem>
             )}
         />

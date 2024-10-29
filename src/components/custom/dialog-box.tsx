2c2434e4c2fc1f87a1541fb265be14de889aae15
paperkraft <paper@kraft.com>
2024-10-29 17:05:06 +0530

    dialog - updated

--- src/components/custom/dialog-box.tsx
+++ src/components/custom/dialog-box.tsx
@@ -3,44 +3,43 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { cn } from "@/lib/utils";
 import React, { memo, useState } from "react";
 
-type DialogProps= {
-    open: boolean,
-    setClose?: () => void;
-    title: string;
-    description?: string;
-    children: React.ReactNode;
-    footer?: React.ReactNode | null;
-    hideClose?: boolean;
-    preventClose?:boolean;
+type DialogProps = {
+  open: boolean,
+  setClose?: () => void;
+  title: string;
+  description?: string;
+  children: React.ReactNode;
+  footer?: React.ReactNode | null;
+  hideClose?: boolean;
+  preventClose?: boolean;
 }
 
-const DialogBox = memo(function DialogBox({open, setClose, title, description, footer, children, ...rest}:DialogProps) {
-    const [opened, setOpen] = useState(open);
+const DialogBox = memo(function DialogBox({ open, title, children, ...rest }: DialogProps) {
+  const [opened, setOpen] = useState(open);
 
-    const handleOpenChange = () => {
-      setOpen(false);
-      setClose?.();
-    }
-    
-    return(
-      <Dialog open={opened} onOpenChange={handleOpenChange}>
-        <DialogContent aria-describedby="content" 
-          className={cn({"[&>button]:hidden": rest.hideClose})}
-          onInteractOutside={(e) => { rest.preventClose && e.preventDefault();}}
-        >
-          <DialogHeader>
-            <DialogTitle>{title ?? "Dialog Title"}</DialogTitle>
-            <>
-            <DialogDescription className="my-4">{description}</DialogDescription>
-              { children } 
-            </>
-          </DialogHeader>
-          <DialogFooter>
-            { footer }
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    )
+  const handleOpenChange = () => {
+    setOpen(false);
+    rest.setClose?.();
+  }
+
+  return (
+    <Dialog open={opened} onOpenChange={handleOpenChange}>
+      <DialogContent aria-describedby="content"
+        className={cn({ "[&>button]:hidden": rest.hideClose })}
+        onInteractOutside={(e) => { rest.preventClose && e.preventDefault() }}
+        onEscapeKeyDown={(e) => { rest.preventClose && e.preventDefault() }}
+      >
+        <DialogHeader>
+          <DialogTitle className="mb-4">{title ?? "Alert"}</DialogTitle>
+          <>
+            {rest.description && <DialogDescription className="my-4">{rest.description}</DialogDescription>}
+            {children}
+          </>
+        </DialogHeader>
+        {rest.footer && <DialogFooter>{rest.footer}</DialogFooter>}
+      </DialogContent>
+    </Dialog>
+  )
 });
 
 export default DialogBox;

5fff71af032c971ca9cbbd194af9cbfbe6f20e2c
paperkraft <royalsv105@gmail.com>
2024-11-25 20:24:29 +0530

    hooks - guard

---
+++ src/hooks/use-module-id.ts
@@ -0,0 +1,31 @@
+import { useSession } from "next-auth/react";
+
+const useModuleIdByName = (moduleName: string): string | null => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return null;
+  }
+
+  if (!session || !session.user || !session.user.modules) {
+    console.warn("Session or modules not found");
+    return null;
+  }
+
+  const findModuleId = (modules: any[]): string | null => {
+    for (const module of modules) {
+      if (module.name === moduleName) {
+        return module.id;
+      }
+
+      if (module.submodules && module.submodules.length > 0) {
+        const submoduleId = findModuleId(module.submodules);
+        if (submoduleId) return submoduleId;
+      }
+    }
+    return null;
+  };
+  return findModuleId(session.user.modules);
+};
+
+export default useModuleIdByName;

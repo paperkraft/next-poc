047276e709addda06c722ff4bab1b8a766a5dcee
SV <royalsv105@gmail.com>
2025-07-12 22:13:55 +0530

    migration: deleted

--- prisma/migrations/20250705132328_is_assigned/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isDefault` on the `RoleWidget` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "RoleWidget" DROP COLUMN "isDefault",
-ADD COLUMN     "isAssigned" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20250709173202_menu_item/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "MenuItem_tenantId_groupId_parentId_key";
-
--- CreateIndex
-CREATE INDEX "UserWidget_userId_idx" ON "UserWidget"("userId");
